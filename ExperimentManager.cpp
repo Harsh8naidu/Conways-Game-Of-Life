ae02f3b493c864b91eea96f663ac52926cf63b77
Harsh8naidu <harsh8naidu@gmail.com>
2025-04-15 23:10:23 +0100

    Check if the pattern found, is stable or not

--- ExperimentManager.cpp
+++ ExperimentManager.cpp
@@ -7,13 +7,30 @@ ExperimentManager::ExperimentManager(const GameConfig& config) : config(config),
 
 bool ExperimentManager::RunExperiement() {
     int attemptCount = 0;
+    int extraAttempts = 5;
     
     while (attemptCount < config.maxAttempts) {
         grid.SimulateAndDisplayStep(300);
 
         if (PatternDetector::ContainsPattern(grid.GetCurrentGrid(), config.pattern)) {
-            std::cout << config.pattern << " found after " << attemptCount << "attempts!\n";
-            return true;
+            // Run extra iterations to check for stable pattern
+            bool patternStable = true;
+            for (int i = 0; i < extraAttempts; i++) {
+                grid.SimulateAndDisplayStep(300);
+                if (!PatternDetector::ContainsPattern(grid.GetCurrentGrid(), config.pattern)) {
+                    std::cout << config.pattern << " disappeared after " << (attemptCount + i + 1) << " steps.\n";
+                    patternStable = false;
+                    break;
+                }
+            }
+
+            if (patternStable) {
+                std::cout << config.pattern << " found after " << attemptCount << " attempts!\n";
+                return true;
+            }
+            else {
+                std::cout << config.pattern << " was found but not stable.\n";
+            }
         }
 
         ++attemptCount;
