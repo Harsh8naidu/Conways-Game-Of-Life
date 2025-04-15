c499f4926257d5aabebb461c8323509efa56c58a
Harsh8naidu <harsh8naidu@gmail.com>
2025-04-15 22:37:17 +0100

    Initialize values  for config

--- GameConfig.h
+++ GameConfig.h
@@ -2,10 +2,10 @@
 #include <string>
 
 struct GameConfig {
-    int rows;
-    int columns;
-    int aliveCells;
-    int steps;
-    std::string pattern;
-    int maxAttempts;
+    int rows = 0;
+    int columns = 0;
+    int aliveCells = 0;
+    int steps = 0;
+    std::string pattern = "";
+    int maxAttempts = 0;
 };
